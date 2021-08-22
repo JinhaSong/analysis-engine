6aac02bb29c755cd2ffb9d031f754853a0dd5093
DESKTOP-MMLAB-O\JinhaSong <jinhasong@sogang.ac.kr>
2021-08-23 01:32:25 +0900

    Revert "Update module(age gender race recognition model)"
    This reverts commit 86d072bee71c33e2155691b04524cb93e1a3e5a5.

--- WebAnalyzer/tasks.py
+++ WebAnalyzer/tasks.py
@@ -25,8 +25,8 @@ def module_load_init(**__):
     #   - Add your model
     #   - You can use worker_index if you need to get and set gpu_id
     #       - ex) gpu_id = worker_index % TOTAL_GPU_NUMBER
-    from Modules.AGR_recognition.main import ARG_recognition
-    analyzer = ARG_recognition()
+    from Modules.dummy.main import Dummy
+    analyzer = Dummy()
 
 
 @app.task
