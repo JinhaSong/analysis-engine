1388dde9173a6bff60ffb820715584689c029f01
Jinha Song <jhsong0729@sogang.ac.kr>
2021-12-18 20:49:30 +0900

    Update media.py

--- WebAnalyzer/utils/media.py
+++ WebAnalyzer/utils/media.py
@@ -74,13 +74,18 @@ def extract_audio(video_url):
 
 def extract_frames(video_url, extract_fps, start_time, end_time):
     frame_dir_path, url = get_video_dir_path(video_url)
-
-    command = "ffmpeg -y -hide_banner -loglevel panic -i {} -ss {} -to {} -vsync 2 -q:v 0 -vf fps={} {}/%05d.jpg".format(
-        video_url, 
-        start_time,
-        end_time,
-        extract_fps, 
-        frame_dir_path)
+    if end_time == "00:00:00.00":
+        command = "ffmpeg -y -hide_banner -loglevel panic -i {} -vsync 2 -q:v 0 -vf fps={} {}/%05d.jpg".format(
+            video_url, 
+            extract_fps, 
+            frame_dir_path)
+    else:
+        command = "ffmpeg -y -hide_banner -loglevel panic -i {} -ss {} -to {} -vsync 2 -q:v 0 -vf fps={} {}/%05d.jpg".format(
+            video_url, 
+            start_time,
+            end_time,
+            extract_fps, 
+            frame_dir_path)
     os.system(command)
 
     framecount = len(os.listdir(frame_dir_path))
